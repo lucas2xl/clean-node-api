0705225f2febcc5a3cf691eddffa9d13c4ac3b06
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-14 14:15:05 -0300

    test: ensure SignUpController return 200 if valid data is provided

--- src/presentation/controllers/signUp/signup.spec.ts
+++ src/presentation/controllers/signUp/signup.spec.ts
@@ -30,9 +30,9 @@ function makeAddAccount(): AddAccount {
     add(account: AddAccountModel): AccountModel {
       const fakeAccount = {
         id: 'valid_id',
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
       };
 
       return fakeAccount;
@@ -222,6 +222,27 @@ describe('SignUp Controller', () => {
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password',
+    });
+  });
+
+  it('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
     });
   });
 });
