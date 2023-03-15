f1a21e33cad962a67844a85f8a2757c089823e7a
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-14 23:27:55 -0300

    chore: remove all index.ts

--- src/presentation/controllers/signUp/signup-protocols.ts
+++
@@ -1,4 +0,0 @@
-export * from '../../../domain/models/account-model';
-export * from '../../../domain/usecases/add-account-usecase';
-export * from '../../protocols';
-export * from '../../protocols/email-validator';
