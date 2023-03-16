f25b9bec51d591beeca30baa66700566d85a2deb
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-16 13:08:48 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.e2e-spec.ts
+++ src/main/routes/signup-routes.e2e-spec.ts
@@ -1,7 +1,21 @@
-import * as request from 'supertest';
+import { MongoHelper } from '@/infra/database/mongodb/helpers/mongo-helper';
 import app from '@/main/config/app';
+import * as request from 'supertest';
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect();
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('account');
+    await accountCollection.deleteMany({});
+  });
+
   it('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
