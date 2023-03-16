3acff674709beafb687da1df4ed2c39009cbbdf1
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-16 11:22:28 -0300

    test: ensure cors is enabled

---
+++ src/main/middlewares/cors.e2e-spec.ts
@@ -0,0 +1,16 @@
+import * as request from 'supertest';
+import app from '@/main/config/app';
+
+describe('CORS Middleware', () => {
+  it('Should enable CORS', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send();
+    });
+
+    await request(app)
+      .get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*');
+  });
+});
