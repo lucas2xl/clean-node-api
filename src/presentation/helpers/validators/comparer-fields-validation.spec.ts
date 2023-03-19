ef17b93a0cd959de532f2376a81d8081477104d9
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-19 14:53:05 -0300

    test: add tests to ComparerField to validation

---
+++ src/presentation/helpers/validators/comparer-fields-validation.spec.ts
@@ -0,0 +1,29 @@
+import { InvalidParamError } from '@/presentation/errors/invalid-param-error';
+import { ComparerFieldsValidation } from '@/presentation/helpers/validators/comparer-fields-validation';
+import { Validation } from '@/presentation/helpers/validators/validation';
+
+function makeSut(): Validation {
+  return new ComparerFieldsValidation('field', 'fieldToCompare');
+}
+
+describe('ComparerField Validation', () => {
+  it('should return a InvalidParamError of validation fails', () => {
+    const sut = makeSut();
+    const error = sut.validate({
+      field: 'any-field',
+      fieldToCompare: 'wrong-field',
+    });
+
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'));
+  });
+
+  it('should not return if validation succeeds', () => {
+    const sut = makeSut();
+    const error = sut.validate({
+      field: 'any-field',
+      fieldToCompare: 'any-field',
+    });
+
+    expect(error).toBeFalsy();
+  });
+});
