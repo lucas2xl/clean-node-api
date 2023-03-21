0e4b55d4483fad529163edb4f54973ea35f126ff
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-21 11:25:07 -0300

    test: add login route

--- src/main/routes/login-routes.e2e-spec.ts
+++ src/main/routes/login-routes.e2e-spec.ts
@@ -1,7 +1,23 @@
+import { AddAccountModel } from '@/domain/usecases/add-account-usecase';
 import { MongoHelper } from '@/infra/database/mongodb/helpers/mongo-helper';
+import { AccountMongoRepository } from '@/infra/database/mongodb/repositories/account/account-mongo-repository';
 import app from '@/main/config/app';
+import { hash } from 'bcrypt';
 import * as process from 'process';
 import * as request from 'supertest';
+
+async function makeAddAccountModel(): Promise<AddAccountModel> {
+  const password = await hash('any-password', 12);
+  return {
+    name: 'any-name',
+    email: 'any-email@mail.com',
+    password,
+  };
+}
+
+function makeAccountMongoRepository(): AccountMongoRepository {
+  return new AccountMongoRepository();
+}
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -30,6 +46,22 @@ describe('Login Routes', () => {
           passwordConfirmation: 'any-password',
         })
 
+        .expect(200);
+    });
+  });
+
+  describe('POST /login', () => {
+    it('Should return 200 on login', async () => {
+      const mongoRepository = makeAccountMongoRepository();
+      const fakeAccount = await makeAddAccountModel();
+      await mongoRepository.add(fakeAccount);
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any-email@mail.com',
+          password: 'any-password',
+        })
         .expect(200);
     });
   });
