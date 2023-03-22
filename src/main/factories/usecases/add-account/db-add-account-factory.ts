a575c95ac6c343d5f7cd2a19d6247499b9660991
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-22 11:47:27 -0300

    refactor: add accountMongoRepository two times in DbAddAccount

--- src/main/factories/usecases/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -6,6 +6,10 @@ import { AccountMongoRepository } from '@/infra/database/mongodb/repositories/ac
 export function makeDbAddAccount(): AddAccount {
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt);
-  const addAccountRepository = new AccountMongoRepository();
-  return new DbAddAccount(bcryptAdapter, addAccountRepository);
+  const accountMongoRepository = new AccountMongoRepository();
+  return new DbAddAccount(
+    bcryptAdapter,
+    accountMongoRepository,
+    accountMongoRepository,
+  );
 }
