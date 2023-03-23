d4c01fd07ce8804f510eee3db520b4de7415b2a1
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-23 17:16:23 -0300

    feat: ensure load surveys route returns 403 if no token is provider

---
+++ src/main/factories/usecases/survey/db-load-surveys-factory.ts
@@ -0,0 +1,8 @@
+import { DbLoadSurveys } from '@/data/usecases/survey/db-load-surveys';
+import { LoadSurveysUsecase } from '@/domain/usecases/load-surveys-usecase';
+import { SurveyMongoRepository } from '@/infra/database/mongodb/repositories/survey/survey-mongo-repository';
+
+export function makeDbLoadSurveys(): LoadSurveysUsecase {
+  const surveyMongoRepository = new SurveyMongoRepository();
+  return new DbLoadSurveys(surveyMongoRepository);
+}
