b59d91e3b7821dc21128b567376fdce63eb676a5
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-23 16:31:33 -0300

    test: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys-controller.ts
@@ -1,12 +1,13 @@
 import { LoadSurveysUsecase } from '@/domain/usecases/load-surveys-usecase';
+import { ok } from '@/presentation/helpers/http/http-helper';
 import { Controller } from '@/presentation/protocols/controller';
-import { HttpRequest, HttpResponse } from '@/presentation/protocols/http';
+import { HttpResponse } from '@/presentation/protocols/http';
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveysUsecase) {}
 
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load();
-    return null;
+  async handle(): Promise<HttpResponse> {
+    const surveys = await this.loadSurveys.load();
+    return ok(surveys);
   }
 }
