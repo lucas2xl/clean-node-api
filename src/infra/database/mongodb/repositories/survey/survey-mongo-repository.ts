3b79c2555309e5b431d02955ccec69371ef04842
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-23 17:03:36 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/database/mongodb/repositories/survey/survey-mongo-repository.ts
+++ src/infra/database/mongodb/repositories/survey/survey-mongo-repository.ts
@@ -1,8 +1,12 @@
 import { AddSurveyRepository } from '@/data/protocols/database/survey/add.survey-repository';
+import { LoadSurveysRepository } from '@/data/protocols/database/survey/load-surveys-repository';
+import { SurveyModel } from '@/domain/models/survey-model';
 import { AddSurveyModel } from '@/domain/usecases/add-survey-usecase';
 import { MongoHelper } from '@/infra/database/mongodb/helpers/mongo-helper';
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository
+  implements AddSurveyRepository, LoadSurveysRepository
+{
   async add(surveyData: AddSurveyModel): Promise<void> {
     const accountCollection = await MongoHelper.instance.getCollection(
       'surveys',
@@ -10,5 +14,13 @@ export class SurveyMongoRepository implements AddSurveyRepository {
 
     const { insertedId } = await accountCollection.insertOne(surveyData);
     await accountCollection.findOne({ _id: insertedId });
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const accountCollection = await MongoHelper.instance.getCollection(
+      'surveys',
+    );
+
+    return accountCollection.find().toArray();
   }
 }
