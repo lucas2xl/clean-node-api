aec2828da3e8f3b494eb3c475d54e47e5dc8eb7b
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-24 13:51:12 -0300

    feat: ensure SaveSurveyResultController returns 500 if LoadSurveyById returns throw

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,7 +1,11 @@
 import { SaveSurveyResultUsecase } from '@/domain/usecases/survey-result/save-survey-result-usecase';
 import { LoadSurveyByIdUsecase } from '@/domain/usecases/survey/load-survey-by-id-usecase';
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error';
-import { forbidden } from '@/presentation/helpers/http/http-helper';
+import {
+  badRequest,
+  forbidden,
+  serverError,
+} from '@/presentation/helpers/http/http-helper';
 import { Controller } from '@/presentation/protocols/controller';
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http';
 import { Validation } from '@/presentation/protocols/validation';
@@ -14,13 +18,22 @@ export class SaveSurveyResultController implements Controller {
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(
-      httpRequest.params.surveyId,
-    );
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'));
-    }
+    try {
+      const data = { ...httpRequest.params, ...httpRequest.body };
+      const error = this.validation.validate(data);
+      if (error) {
+        return badRequest(error);
+      }
 
-    return Promise.resolve(undefined);
+      const { surveyId } = httpRequest.params;
+      const survey = await this.loadSurveyById.loadById(surveyId);
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'));
+      }
+
+      return Promise.resolve(undefined);
+    } catch (e) {
+      return serverError(e);
+    }
   }
 }
