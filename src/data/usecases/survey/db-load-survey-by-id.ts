0828b1ff81fc61e97207b24aa567413c9e81ca96
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-23 21:55:05 -0300

    feat: ensure DbLoadSurveyByIdUsecase returns survey on success

--- src/data/usecases/survey/db-load-survey-by-id.ts
+++ src/data/usecases/survey/db-load-survey-by-id.ts
@@ -8,7 +8,6 @@ export class DbLoadSurveyById implements LoadSurveyByIdUsecase {
   ) {}
 
   async loadById(id: string): Promise<SurveyModel> {
-    await this.loadSurveyByIdRepository.loadById(id);
-    return null;
+    return this.loadSurveyByIdRepository.loadById(id);
   }
 }
