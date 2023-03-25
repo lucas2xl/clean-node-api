1837c061e75dfeb3c870d81651b85ebb00dfe3bd
Lucas Aguiar <luqkinhaas@gmail.com>
2023-03-25 11:19:43 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import setupMiddlewares from './middlewars';
 import setupRoutes from './routes';
+import setupSwagger from './swagger';
 
 const app = express();
+setupSwagger(app);
 setupMiddlewares(app);
 setupRoutes(app);
 
